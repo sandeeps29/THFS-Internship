4666187446385e6d4bc87381bef6c03ee7589d09
sandeeps29 <sandeep12345santhosh@gmail.com>
2024-06-02 17:56:54 +0530

    cart js and amazon js updated

--- JavaScript/Advanced JavaScript/Amazon Project/data/cart.js
+++ JavaScript/Advanced JavaScript/Amazon Project/data/cart.js
@@ -1,1 +1,18 @@
 export const cart = [];
+
+export function addToCart(productId){
+  let similarItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      similarItem = cartItem;
+    }
+  });
+  if (similarItem) {
+    similarItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
