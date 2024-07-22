cdc0596e0bf8f124f8dec9c830ac359fed106221
sandeeps29 <sandeep12345santhosh@gmail.com>
2024-07-22 15:31:38 +0530

    use effect hook learned

--- React/Grocery-List/src/App.js
+++ React/Grocery-List/src/App.js
@@ -1,43 +1,42 @@
 import Footer from "./Footer";
 import Content from "./Content";
 import Header from "./Header";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddItem from "./AddItem";
 import SearchItem from "./SearchItem";
 
 function App() {
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppinglist"))
+    JSON.parse(localStorage.getItem("shoppinglist")) || []
   );
 
   const [newItem, setNewItem] = useState("");
 
   const [search, setSearch] = useState("");
 
-  const setAndSaveItem = (newItems) => {
-    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
-    setItems(newItems);
-  };
+  useEffect(() => {
+    localStorage.setItem("shoppinglist", JSON.stringify(items));
+  }, [items]);
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItem(listItems);
+    setItems(listItems);
   };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) => {
       return item.id === id ? { ...item, checked: !item.checked } : item;
     });
-    setAndSaveItem(listItems);
+    setItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => {
       return item.id !== id;
     });
-    setAndSaveItem(listItems);
+    setItems(listItems);
   };
   const handleSubmit = (event) => {
     event.preventDefault();
